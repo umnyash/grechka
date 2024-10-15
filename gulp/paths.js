8cb2e2d8f81f60a46fb8cf1d652c9c2cdec83e64
Rustam Sadreev <rustam17.sb@gmail.com>
2024-10-15 17:06:43 +0300

    init: add site files

---
+++ gulp/paths.js
@@ -0,0 +1,68 @@
+const pathSrc = 'source';
+const pathDest = 'build';
+
+export default {
+  root: pathDest,
+
+  copyAssets: {
+    src: [
+      `${pathSrc}/img/**/*.webp`,
+      `${pathSrc}/fonts/**/*.*`,
+      `${pathSrc}/*.ico`,
+      `${pathSrc}/*.webmanifest`,
+      `${pathSrc}/vendor/**/*.*`,
+    ],
+    watch: [
+      `${pathSrc}/img/**/*.webp`,
+      `${pathSrc}/fonts/**/*.*`,
+      `${pathSrc}/*.ico`,
+      `${pathSrc}/*.webmanifest`,
+    ],
+    dest: pathDest,
+    base: pathSrc
+  },
+
+  createVectorStack: {
+    src: `${pathSrc}/img/icons/**/*.svg`,
+    watch: `${pathSrc}/img/icons/**/*.svg`,
+    dest: `${pathDest}/img`
+  },
+
+  createWebp: {
+    src: `${pathSrc}/img/**/*.{png,jpg}`,
+    watch: `${pathSrc}/img/**/*.{png,jpg}`,
+    dest: `${pathDest}/img`
+  },
+
+  optimizeVector: {
+    src: [
+      `${pathSrc}/img/**/*.svg`,
+      `!${pathSrc}/img/icons/**/*.svg`
+    ],
+    watch: [
+      `${pathSrc}/img/**/*.svg`,
+      `!${pathSrc}/img/icons/**/*.svg`
+    ],
+    dest: `${pathDest}/img`
+  },
+
+  processMarkup: {
+    src: `${pathSrc}/pug/*.pug`,
+    watch: `${pathSrc}/pug/**/*.pug`,
+    dest: pathDest,
+  },
+
+  processScripts: {
+    src: [
+      `${pathSrc}/js/main.js`,
+    ],
+    watch: `${pathSrc}/js/**/*.js`,
+    dest: `${pathDest}/js`
+  },
+
+  processStyles: {
+    src: `${pathSrc}/sass/*.scss`,
+    watch: `${pathSrc}/sass/**/*.scss`,
+    dest: `${pathDest}/css`
+  },
+};
